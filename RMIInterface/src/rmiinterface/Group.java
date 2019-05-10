7b23b93853a77024f72854141b048ba14508d244
Tran Hoang Anh <43133834+hoanganh996@users.noreply.github.com>
2019-05-10 10:18:24 +0700

    group chat and user class update

---
+++ RMIInterface/src/rmiinterface/Group.java
@@ -0,0 +1,44 @@
+package rmiinterface;
+
+import java.io.Serializable;
+
+public class Group implements Serializable{
+    private int id;
+    private String name;
+    private String password;
+
+    public Group() {
+    }
+
+    public Group(int id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
